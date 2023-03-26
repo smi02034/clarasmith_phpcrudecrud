d80fc2684bc28962245268dea26b874e8b99a15d
smi02034 <smi02034@umn.edu>
2023-03-26 22:09:35 +0000

    first commit

--- credentials.php
+++ credentials.php
@@ -1,6 +1,6 @@
 <?php                            //connection & credentials
 $servername = "localhost";      //mysql is on the same host as apache
 $dbname = "employees";          //which db you're going to use
-$username = "phpuser1";
-$password = "abc123";
+$username = "clara";
+$password = "2868";
 ?>
